3273192c6be81de4fb47cd993565abe8939a4003
Paloma LORENZON <Palomalorenzon1993@gmail.com>
2026-09-23 15:50:44 -0300

    Config do Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// Cole aqui o firebaseConfig do seu projeto:
-// Firebase → ⚙ Configurações do projeto → Seus apps → Web (</>)
+// Configuração do projeto Firebase (minhas-financas-cee4e).
+// Estes valores não são senha: quem protege os dados são as regras do Firestore.
 window.FIREBASE_CONFIG = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyAJ3bRu6_yVs6IwOv98SFOrP6de2X6BcpE",
+  authDomain: "minhas-financas-cee4e.firebaseapp.com",
+  projectId: "minhas-financas-cee4e",
+  storageBucket: "minhas-financas-cee4e.firebasestorage.app",
+  messagingSenderId: "434988135908",
+  appId: "1:434988135908:web:55df39ff2a20ac3430c3f1"
 };
